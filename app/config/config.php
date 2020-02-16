23f6b58d744c907238662b25570d6d4ab6f37462
Joe Henson <jd.henson@me.com>
2020-02-16 22:44:22 +0000

    Define main configuration file; DB connections to utilise getenv() in future

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_USERNAME');
+define('DB_PASS', 'YOUR_PASSWORD');
+define('DB_NAME', 'YOUR_DB_NAME');
+
+// App root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URL_ROOT', 'http://localhost:8888/');
+
+// Site name
+define('SITE_NAME', 'JHP Framework');
