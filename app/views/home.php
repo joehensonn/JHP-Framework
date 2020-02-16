b1735f8fcc3476f68912993ee8dc5cce629ade4c
Joe Henson <jd.henson@me.com>
2020-02-16 22:50:31 +0000

    Finally... welcome to JHP Framework

---
+++ app/views/home.php
@@ -0,0 +1,4 @@
+<h1>Welcome to the JHP Framework.</h1>
+
+<p>Authored by <strong>Joe Henson</strong></p>
+<p><a href="https://github.com/joehensonn" target="_blank" rel="noopener noreferrer">Github</a></p>
